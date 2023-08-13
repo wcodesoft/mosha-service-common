dcb6369a274801bab8a98225928df75f1c8a699b
Walter <walterjgsp@gmail.com>
2023-08-12 23:07:18 -0400

    Adding base definition for a HttpService to be used through all services.

--- http/http.go
+++ http/http.go
@@ -2,7 +2,10 @@ package http
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/log"
 	"net/http"
+	"time"
 )
 
 // EncodeResponse encodes the response to JSON and writes it to the response writer
@@ -16,4 +19,27 @@ func EncodeResponse(w http.ResponseWriter, response interface{}) {
 func EncodeError(w http.ResponseWriter, response error) {
 	w.WriteHeader(http.StatusInternalServerError)
 	EncodeResponse(w, response.Error())
+}
+
+// MoshaHttpService is an interface for any Mosha HTTP service.
+type MoshaHttpService interface {
+	MakeHandler() http.Handler
+	GetPort() string
+	GetName() string
+}
+
+// StartHttpService starts a Mosha HTTP service.
+func StartHttpService(mhs MoshaHttpService) error {
+	log.Infof("Starting %s http on %s", mhs.GetName(), mhs.GetPort())
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%s", mhs.GetPort()),
+		Handler:           mhs.MakeHandler(),
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		return fmt.Errorf("unable to start service %q: %s", mhs.GetName(), err)
+	}
+	return nil
 }
